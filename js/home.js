a8a6dcf529ff53c15f743fcfdcc71e4a2c3c305d
JustSySy <1287661205@qq.com>
2020-05-21 14:59:47 +0800

    初始化页面

--- js/home.js
+++ js/home.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
+  var hash = location.hash
+  var path = []
+  if(!!hash){
+    path = hash.substring(1,hash.length).split("/")
+    path = path.slice(2,path.length)
+  }
   $("#tree-container").tagTree({
     id:"",
     data:window.treeMenue,
     fold:true,
+    openName:path,
     multiple:false,
     click:function(val){
       val && val != 'undefined' && renderMarkEvent(val)
