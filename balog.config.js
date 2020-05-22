aeb298e108b483748049cc773d780b5aa87ffe3a
JustSySy <1287661205@qq.com>
2020-05-22 09:42:46 +0800

    优化构建

---
+++ balog.config.js
@@ -0,0 +1,6 @@
+module.exports={
+  style:'light',
+  title:'blog',
+  logo:'',
+  template:'/index.html'
+}
---
+++ build/index.js
@@ -0,0 +1,18 @@
+function replaceTmpParamter(htmlStr,name,value) {
+  return htmlStr.replace(new RegExp(`<\\%\\=\\s*${name}\\s*\\%>`,'g'),value)
+}
+
+function readTemplate(path) {
+  
+}
+
+function buildCss(style) {
+  
+}
+
+function buildMeta() {
+  
+}
+module.exports=function buildTemplate(config = {}) {
+  
+}
