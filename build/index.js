eb6de4aac6f0c2f1120a076a20c3cfded628867c
JustSySy <1287661205@qq.com>
2020-05-22 10:33:12 +0800

    构建初始化

--- build/index.js
+++ build/index.js
@@ -1,18 +1,32 @@
+const fs = require('fs')
+const path=require('path');
+var projectDir=path.join(__dirname,'../');
 function replaceTmpParamter(htmlStr,name,value) {
   return htmlStr.replace(new RegExp(`<\\%\\=\\s*${name}\\s*\\%>`,'g'),value)
 }
 
 function readTemplate(path) {
-  
+  const templatePath = path.join(projectDir,path)
+  return fs.readFileSync(templatePath).toString()
 }
 
-function buildCss(style) {
+function buildStyle(style) {
   
 }
 
 function buildMeta() {
   
 }
+
+function buildJS() {
+  
+}
+
+function exportHtml(htmlStr,outdir){
+  const outPath = path.join(projectDir,outdir)
+  fs.writeFileSync(outPath, htmlStr)
+}
+
 module.exports=function buildTemplate(config = {}) {
   
 }
